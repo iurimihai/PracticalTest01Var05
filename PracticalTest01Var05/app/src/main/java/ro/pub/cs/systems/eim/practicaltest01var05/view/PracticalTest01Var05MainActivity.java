da0641d3663441e7896f5c01da4c27ae13c2deb8
Meresan Iuri-Mihai <meresan.iurimihai@gmail.com>
2021-04-15 12:07:08 +0300

    A.2
    Signed-off-by: Meresan Iuri-Mihai <meresan.iurimihai@gmail.com>

--- PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/view/PracticalTest01Var05MainActivity.java
+++ PracticalTest01Var05/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/view/PracticalTest01Var05MainActivity.java
@@ -3,10 +3,15 @@ package ro.pub.cs.systems.eim.practicaltest01var05.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ro.pub.cs.systems.eim.practicaltest01var05.R;
 
 public class PracticalTest01Var05MainActivity extends AppCompatActivity {
+
+    private Button topLeft, topRight, center, bottomLeft, bottomRight;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
